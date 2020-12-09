17e313321075daaadec0521ed37fdb53986594ab
Aleksandar <aleksandarlilic03@gmail.com>
2020-12-09 06:58:39 +0100

    p_03 update v0.2

--- p_03/software/sw_alarm_clock/main.c
+++ p_03/software/sw_alarm_clock/main.c
@@ -31,7 +31,7 @@ typedef struct{
     // switch status
     mode_type sw_mode_time_set;
     settings_type sw_set_time_alarm;
-    alt_u8 sw_alarm_active;
+    alt_u8 sw_alarm_enable;
     // btn status
     alt_u8 btn_h;
     alt_u8 btn_m;
@@ -44,10 +44,12 @@ typedef struct{
     alt_u8 alarm_h;
     alt_u8 alarm_m;
     alt_u8 alarm_s;
+    // alarm active flag
+    alt_u8 alarm_active;
     // chag flags time
-    // alt_u8 chg_h;
-    // alt_u8 chg_m;
-    // alt_u8 chg_s;
+    alt_u8 chg_h;
+    alt_u8 chg_m;
+    alt_u8 chg_s;
     // counter enable and status jtag
     alt_u8 jtag_en;
     alt_u8 jtag_time_matched;
@@ -64,24 +66,86 @@ typedef struct{
 * argument:
 *   btn_base: base address of pushbutton PIO
 *   timer_base: base address of user timer
+*   ledr_base: base address of ledr pio
+*   status: pointer to status struct
 * return:
 * note:
 ***********************************************************************/
-void flashsys_init_v1(alt_u32 btn_base, alt_u32 timer_base, status_struct *status)
+void flashsys_init_v1(alt_u32 btn_base, alt_u32 timer_base, alt_u32 ledr_base, status_struct *status)
 {
     btn_clear(btn_base);              // clear button edge-capture reg
     timer_wr_per(timer_base, 50000);  // set 1-ms timeout period
-    status -> time_h = 0;
+    pio_write(ledr_base, 0);
     status -> sw_mode_time_set = mode_time;
     status -> sw_set_time_alarm = set_time;
+    status -> time_h = 0;
     status -> time_m = 59;
     status -> time_s = 0;
     status -> alarm_h = 0;
-    status -> alarm_m = 0;
-    status -> alarm_s = 0;  
-    // status -> chg_h = 0;  
-    // status -> chg_m = 0;  
-    // status -> chg_s = 0;  
+    status -> alarm_m = 59;
+    status -> alarm_s = 5;
+    status -> alarm_active = 0;    
+    status -> chg_h = 0;  
+    status -> chg_m = 0;  
+    status -> chg_s = 0;  
+}
+/***********************************************************************
+* function: check_btn()
+* purpose:  check value of buttons and update struct
+* argument:
+*   btn: button value read from inputs
+*   status: pointer to status struct
+* return:
+*   updated status
+* note:
+***********************************************************************/
+void check_btn(alt_u8 btn, status_struct *status)
+{
+    if (btn & 0x01){                           // button 0 pressed    
+    status -> chg_s = 1;
+        if (status -> sw_set_time_alarm == set_time){
+            status -> time_s ++;
+            if (status -> time_s == 60){
+                status -> time_s = 0;
+            }
+        }        
+        else{
+            status -> alarm_s ++;
+            if (status -> alarm_s == 60){
+                status -> alarm_s = 0;
+            }            
+        }
+    }
+    if (btn & 0x02){                           // button 1 pressed
+    status -> chg_m = 1;
+        if (status -> sw_set_time_alarm == set_time){
+            status -> time_m ++;
+            if (status -> time_m == 60){
+                status -> time_m = 0;
+            }
+        }        
+        else{
+            status -> alarm_m ++;
+            if (status -> alarm_m == 60){
+                status -> alarm_m = 0;
+            }            
+        }
+    }
+    if (btn & 0x04){                           // button 2 pressed
+    status -> chg_h = 1;
+        if (status -> sw_set_time_alarm == set_time){
+            status -> time_h ++;
+            if (status -> time_h == 24){
+                status -> time_h = 0;
+            }
+        }        
+        else{
+            status -> alarm_h ++;
+            if (status -> alarm_h == 24){
+                status -> alarm_h = 0;
+            }            
+        }
+    }
 }
 
 /***********************************************************************
@@ -90,21 +154,22 @@ void flashsys_init_v1(alt_u32 btn_base, alt_u32 timer_base, status_struct *statu
 * argument:
 *   btn_base: base address of pushbutton PIO
 *   sw_base: base address of switch PIO
-*   cmd: pointer to command
+*   status: pointer to status struct
 * return:
-*   updated cmd
+*   updated status
 * note:
 ***********************************************************************/
 void sw_get_command_v1(alt_u32 btn_base, alt_u32 sw_base, status_struct *status)
 {
     alt_u8 btn;
     alt_u8 sw;
+    //status_struct *ptr_status = status;
   
     sw = (alt_u8) pio_read(sw_base) & 0x7;    // read 3 switches
     if (sw & 0x01)
-        status -> sw_alarm_active = 1;
+        status -> sw_alarm_enable = 1;
     else
-        status -> sw_alarm_active = 0;
+        status -> sw_alarm_enable = 0;
   
     if (sw & 0x02)
         status -> sw_set_time_alarm = set_alarm;
@@ -119,28 +184,12 @@ void sw_get_command_v1(alt_u32 btn_base, alt_u32 sw_base, status_struct *status)
     if (status -> sw_mode_time_set == mode_settings){
         btn = (alt_u8) btn_read(btn_base) & 0x7;    // read 3 pushbuttons
         btn_clear(btn_base);
-        if (btn!=0){                                // a button pressed
-            if (btn & 0x01){                           // button 0 pressed
-                if (status -> sw_set_time_alarm == set_time)
-                    status -> time_s ++;
-                else
-                    status -> alarm_s ++;
-            }            
-            if (btn & 0x02){                           // button 1 pressed
-                if (status -> sw_set_time_alarm == set_time)
-                    status -> time_m ++;
-                else
-                    status -> alarm_m ++;
-            }            
-            if (btn & 0x04){                           // button 2 pressed
-                if (status -> sw_set_time_alarm == set_time)
-                    status -> time_h ++;
-                else
-                    status -> alarm_h ++;
-            }
+        if (btn!=0){                                 // a button pressed
+            check_btn(btn, status);
         }
-    } check for overflow
+    }
 }
+
 
 /***********************************************************************
 * function: jtag_uart_disp_msg_v1()
@@ -224,19 +273,24 @@ void ssd_disp_msg_v1(status_struct *status)
 * function: timer_inc()
 * purpose:  increase time
 * argument:
-*   *status: struct_status with data with time value
+*   *status: struct_status with data for time value
 * return:
 * note:
 ***********************************************************************/
 void timer_inc(status_struct *status, alt_u32 timer_base)
 {
+    static alt_u8 timer_fired = 0;
     static alt_u16 time_ms = 0;
+    static alt_u16 alarm_ms = 0;
+    
+    if (timer_read_tick(timer_base) == 1){
+            timer_clear_tick(timer_base);
+            timer_fired = 1;
+    }
     // stop counter if current time has to be set
     if(!((status -> sw_mode_time_set == mode_settings) & (status -> sw_set_time_alarm == set_time))){
-        if (timer_read_tick(timer_base) == 1){
-            timer_clear_tick(timer_base);
+        if(timer_fired)
             time_ms++;
-        } // end if
         if(time_ms == 1000){
             status -> time_s ++;
             // status -> chg_s = 1;
@@ -256,18 +310,56 @@ void timer_inc(status_struct *status, alt_u32 timer_base)
             status -> time_h = 0;
         } // end if
     }
+    
+    if(status -> alarm_active){
+        if(timer_fired){
+            alarm_ms++;
+        }
+        if(alarm_ms >= 5000){
+            status -> alarm_active = 0;
+            alarm_ms = 0;
+        }
+    }
+    timer_fired = 0;
+}
+
+/***********************************************************************
+* function: alarm_check()
+* purpose:  compare values, if equal fire alarm
+* argument:
+*   *status: struct_status with data for time value
+* return:
+* note:
+***********************************************************************/
+void alarm_check(status_struct *status)
+{
+    if(!(status -> alarm_active)){
+        alt_u8 eq_h = status -> time_h == status -> alarm_h;
+        alt_u8 eq_m = status -> time_m == status -> alarm_m;
+        alt_u8 eq_s = status -> time_s == status -> alarm_s;
+        
+        if(eq_h & eq_m & eq_s){
+            status -> alarm_active = 1;
+        }
+    }
 }
 
 int main(){
     status_struct status;
     // initial settings
-    flashsys_init_v1(BTN_BASE, USR_TIMER_BASE, &status);
+    flashsys_init_v1(BTN_BASE, USR_TIMER_BASE, LEDR_BASE, &status);
     
     //main loop    
     while(1){
-        sw_get_command_v1(BTN_BASE, SW_BASE ,&status);
+        sw_get_command_v1(BTN_BASE, SW_BASE, &status);
         timer_inc(&status, USR_TIMER_BASE); 
         ssd_disp_msg_v1(&status);
+        if(status.sw_alarm_enable){
+            alarm_check(&status);
+        }
+            pio_write(LEDR_BASE, status.alarm_active);
+       // }
+        
     }    
 }// main
 
